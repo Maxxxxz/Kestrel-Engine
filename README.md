3ab28c25d4eade49344f61c0bb69dea56273a0bc
Michael Cooper <mc454916@ohio.edu>
2020-09-18 00:42:45 -0400

    Ticks
    Need to move to threads

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This is the repo for the Kestrel Engine.
 
+# todo
+* finish basic loop with proper ticking (for use later)
+* gather keypresses
+* get mouse information
+* begin graphics 😳
+
 # Dependencies
 * [Ash](https://github.com/MaikKlein/ash) maybe? it is unsafe
 * [Volkano](https://github.com/vulkano-rs/vulkano) is safe, maybe use this?
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use std::time::{Duration, Instant};
+use std::thread;
+
+const ONE_SECOND_IN_MILLISECONDS: u64 = 1000;
+
+// Updates per second
+const PHYS_TICK: i8 = 20;
+const TICK_TIME: i8 = 20;
+
+
 fn main()
 {
+    // move ticks to separate threads so I can use sleep(ONE_SECOND_IN_MICROSECONDS/PHYS_TICK)
+    let mut oldNow = Instant::now();
     loop
     {
-        println!("Hello, temporary gameplay loop");
+        thread::sleep(Duration::from_millis(ONE_SECOND_IN_MILLISECONDS/TICK_TIME as u64));
+
+        let t = Instant::now().duration_since(oldNow);
+        println!("ticked: {:?}", t);
+
+
+
+        oldNow = Instant::now();
     }
 
 }
